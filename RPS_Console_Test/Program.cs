4b7d0eb1ca0be2b1646e80d77843d87f7866867a
jjorgensent <j.jorgensent@gmail.com>
2016-01-22 16:41:58 -0700

    testing out Rock Paper Scissors Hand - Initial commit

---
+++ RPS_Console_Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using Leap;
+using System;
+
+class Sample
+{
+    public statis void Main()
+    {
+        Controller controller = new Controller();
+
+        //keep this process running until Enter is pressed
+        Console.WriteLine("Press Enter to quit...");
+        Console.ReadLine();
+    }
+}
+
+
